f4a91dc208f5674cf40b19406e3f01cf16ffb1c7
JacobJEdwards <jacob.john1@talktalk.net>
2024-01-31 19:35:01 +0000

    see above

---
+++ client/src/views/Schedule.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+import Calendar from "../components/Calendar.vue";
+</script>
+
+<template>
+    <div>
+        <h1>Calendar</h1>
+        <Calendar />
+    </div>
+</template>
