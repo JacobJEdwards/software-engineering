2f3b3d98b2eff644587b9189be8ffabd3400ce18
JacobJEdwards <jacob.john1@talktalk.net>
2024-01-31 17:34:56 +0000

    vite setup

---
+++ client/src/App.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import HelloWorld from './components/HelloWorld.vue'
+import Calendar from './components/Calender.vue'
+</script>
+
+<template>
+  <div>
+  </div>
+  <HelloWorld msg="" />
+  <Calendar />
+</template>
+
+<style scoped>
+</style>
---
+++ client/src/router.ts
@@ -0,0 +1,13 @@
+import { createRouter, createWebHistory } from 'vue-router'
+
+const routes = [
+    {
+        path: '/', 
+        component: () => import("views/Home.vue")
+    },
+]
+
+export const router = createRouter({
+    history: createWebHistory(),
+    routes
+})
