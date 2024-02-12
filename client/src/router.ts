4f1d833434c8775f5f9b56a9658e5b0e3eafd1c3
JacobJEdwards <jacob.john1@talktalk.net>
2024-02-12 10:19:00 +0000

    fixed missing comma

--- client/src/router.ts
+++ client/src/router.ts
@@ -3,7 +3,7 @@ import { createRouter, createWebHistory } from 'vue-router'
 const routes = [
     {
         path: '/',
-        name: "Home"
+        name: "Home",
         component: () => import("./views/Home.vue")
     },
     {
