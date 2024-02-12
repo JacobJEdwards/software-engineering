299eef29784f57cb71076e71692ba07ff2ff6598
jamie <jamie.wales.developer@gmail.com>
2024-02-12 13:14:14 +0000

    models

--- backend/middleware/AuthMiddleware.js
+++ backend/middleware/AuthMiddleware.js
@@ -3,7 +3,7 @@ import jwt from 'jsonwebtoken';
 class AuthMiddleware {
     static authenticate(req, res, next) {
         try {
-            const token = req.headers.authorization.split(" ")[1];
+            const token = req.headers.authorization;
             req.userData = jwt.verify(token, process.env.JWT_SECRET);
             next();
         } catch (e) {
