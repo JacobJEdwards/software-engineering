2f5c10640d2e60d92757b4c473081dc2a3e7216b
Jacob Edwards <jacob.john1@talktalk.net>
2024-04-16 16:38:38 +0100

    update task

--- client/src/components/Task.vue
+++ client/src/components/Task.vue
@@ -10,13 +10,33 @@ const { loading } = useLoading()
 const authStore = useAuthStore()
 const userStore = useUserStore()
 
+type TaskForm = {
+  title: string
+  startDate: string
+  endDate: string
+  status: TaskStatus
+  hrsCompleted: number
+  hrsRequired: number
+}
+
 const props = defineProps<{
   task: Task
   small?: boolean
   editable?: boolean
 }>()
 
+const formData = ref<TaskForm>(<TaskForm>{
+  title: props.task.title,
+  startDate: props.task.startDate,
+  endDate: props.task.endDate,
+  status: props.task.status,
+  hrsCompleted: props.task.hrsCompleted,
+  hrsRequired: props.task.hrsRequired
+})
+
+
 const modelVisible = ref<boolean>(false)
+const edit = ref<boolean>(false)
 
 const deleteTask = async () => {
   loading.value = true
@@ -50,6 +70,46 @@ const deleteTask = async () => {
   }
 }
 
+const updateTask = async () => {
+  loading.value = true
+  try {
+    const taskId = props.task._id
+    const body = {
+      taskId,
+      title: formData.value.title,
+      startDate: formData.value.startDate,
+      endDate: formData.value.endDate,
+      progress: formData.value.status,
+      hrsCompleted: formData.value.hrsCompleted,
+      hrsRequired: formData.value.hrsRequired
+    }
+
+    const response = await fetch(`${API_ROUTE}/protected/task`, {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+        "Authorization": authStore.token ?? ""
+      },
+      body: JSON.stringify(body)
+    })
+
+    if (!response.ok) {
+      throw new Error("Failed to update task")
+    }
+
+    const data = await response.json()
+    console.log(data)
+
+    await userStore.getUser()
+  } catch (error) {
+    console.error(error)
+  } finally {
+    loading.value = false
+  }
+
+}
+
+
 </script>
 
 <template>
@@ -58,19 +118,19 @@ const deleteTask = async () => {
       <v-card-title>
         {{ task.title }}
       </v-card-title>
-      <v-card-text v-if="props.editable">
+      <v-card-text v-if="props.editable && edit">
         <v-row>
           <v-col cols="12">
-            <v-text-field v-model="task.title" label="Title"></v-text-field>
+            <v-text-field v-model="formData.title" label="Title"></v-text-field>
           </v-col>
           <v-col cols="12">
-            <v-text-field v-model="task.startDate" label="Start Date"></v-text-field>
+            <v-text-field v-model="formData.startDate" label="Start Date"></v-text-field>
           </v-col>
           <v-col cols="12">
-            <v-text-field v-model="task.endDate" label="End Date"></v-text-field>
+            <v-text-field v-model="formData.endDate" label="End Date"></v-text-field>
           </v-col>
           <v-col cols="12">
-            <v-select v-model="task.status" label="Status" :items="Object.values(TaskStatuses)"></v-select>
+            <v-select v-model="formData.status" label="Status" :items="Object.values(TaskStatuses)"></v-select>
           </v-col>
           <v-col cols="12">
             <v-text-field hint="Please log an activity to update hours." disabled v-model="task.hrsCompleted"
@@ -78,24 +138,41 @@ const deleteTask = async () => {
                           persistent-hint></v-text-field>
           </v-col>
           <v-col cols="12">
-            <v-text-field v-model="task.hrsRequired" label="Hours Required"></v-text-field>
+            <v-text-field v-model="formData.hrsRequired" label="Hours Required"></v-text-field>
           </v-col>
         </v-row>
       </v-card-text>
       <v-card-text v-else>
-        <v-list>
-          <v-list-item title="Title" :subtitle="task.title"></v-list-item>
-          <v-list-item title="Start Date" :subtitle="task.startDate"></v-list-item>
-          <v-list-item title="End Date" :subtitle="task.endDate"></v-list-item>
-          <v-list-item title="Status" :subtitle="task.status"></v-list-item>
-          <v-list-item title="Hours Completed" :subtitle="task.hrsCompleted"></v-list-item>
-          <v-list-item title="Hours Required" :subtitle="task.hrsRequired"></v-list-item>
-        </v-list>
+        <v-row>
+          <v-col cols="12">
+            <v-list-item title="Title" :key="task._id" :subtitle="task.title"></v-list-item>
+          </v-col>
+          <v-col cols="12">
+            <v-list-item title="Start Date" :key="task._id" :subtitle="task.startDate"></v-list-item>
+          </v-col>
+          <v-col cols="12">
+            <v-list-item title="End Date" :key="task._id" :subtitle="task.endDate"></v-list-item>
+          </v-col>
+          <v-col cols="12">
+            <v-list-item title="Status" :key="task._id" :subtitle="task.status"></v-list-item>
+          </v-col>
+          <v-col cols="12">
+            <v-list-item title="Hours Completed" :key="task._id" :subtitle="task.hrsCompleted"></v-list-item>
+          </v-col>
+          <v-col cols="12">
+            <v-list-item title="Hours Required" :key="task._id" :subtitle="task.hrsRequired"></v-list-item>
+          </v-col>
+        </v-row>
       </v-card-text>
 
       <v-card-actions v-if="props.editable">
-        <v-btn @click="modelVisible = false">Close</v-btn>
-        <v-btn @click="modelVisible = false" color="green">Save</v-btn>
+        <v-btn @click="edit ? edit = false : modelVisible = false" color="blue">
+          {{ edit ? "Cancel" : "Close" }}
+        </v-btn>
+
+        <v-btn @click="edit ? edit = false : edit = true" color="green">
+          {{ edit ? "Save" : "Edit" }}
+        </v-btn>
         <v-spacer></v-spacer>
         <v-btn @click="deleteTask" color="danger" >Delete</v-btn>
       </v-card-actions>
