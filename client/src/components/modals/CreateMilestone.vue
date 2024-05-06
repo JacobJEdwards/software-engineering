0ee29c32fa13e3eb47226f00b3ef558188f1308c
Jacob Edwards <jacob.john1@talktalk.net>
2024-05-06 15:02:04 +0100

    prelim files

---
+++ client/src/components/modals/CreateMilestone.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import { useAuthStore, useUserStore } from "../../stores";
+import { ref, watch } from "vue";
+import { ActivitiesService } from "../../services";
+import { useLoading, useSuccessErrorMessage } from "../..//utils/utils.ts";
+import {
+  TaskStatuses,
+  ActivityForm,
+  ActivityTypes,
+} from "../../typings/user.ts";
+import Alert from "../utils/Alert.vue";
+import NumberInput from "../utils/NumberInput.vue";
+</script>
+
+<template></template>
+
+<style scoped></style>
