ccd31ec36a06943f8dcd98fb7d5e450893f4f4b4
Jamie Wales <jamie.wales.developer@gmail.com>
2024-05-15 20:42:13 +0100

    fix activity bug

--- backend/app.js
+++ backend/app.js
@@ -2,7 +2,7 @@ import dotenv from "dotenv";
 import Application from "./Application.js";
 
 dotenv.config({ path: "../.env",  });
-const app = new Application(3000, 'mongodb://localhost:27017/wonderfultasksdb' );
+const app = new Application(3001, 'mongodb://localhost:27017/wonderfultasksdb' );
 app.start();
 
 /*
